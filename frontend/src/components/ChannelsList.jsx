dd8db2cadbd90eaccc673d58e2d5e72f2b3320f5
botti4elli <apple-003@mail.ru>
2025-06-09 01:09:51 +0400

    _

--- frontend/src/components/ChannelsList.jsx
+++ frontend/src/components/ChannelsList.jsx
@@ -246,127 +246,6 @@
 // }
 //
 // export default ChannelsList
-// import React from 'react'
-// import {
-//   ListGroup, Button, Dropdown,
-// } from 'react-bootstrap'
-// import { BsCaretDownFill, BsThreeDotsVertical } from 'react-icons/bs'
-// import { useDispatch } from 'react-redux'
-// import { useTranslation } from 'react-i18next'
-// import { setCurrentChannelId } from '../slices/channelsSlice'
-//
-// const ChannelsList = ({
-//   channels, currentChannelId, onAddChannel, onRename, onRemove,
-// }) => {
-//   const { t } = useTranslation()
-//   const dispatch = useDispatch()
-//
-//   const handleSelectChannel = (id, e) => {
-//     if (!e.target.closest('.dropdown')) {
-//       dispatch(setCurrentChannelId(id))
-//     }
-//   }
-//
-//   const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-//     <a
-//       href="#"
-//       ref={ref}
-//       onClick={(e) => {
-//         e.preventDefault()
-//         e.stopPropagation()
-//         onClick(e)
-//       }}
-//       className="text-decoration-none text-secondary p-1"
-//     >
-//       <span className="visually-hidden">{t('channel.manage')}</span>
-//       <span className="ms-1 text-white"><BsCaretDownFill size="12" /></span>
-//       {children}
-//     </a>
-//   ))
-//
-//   return (
-//     <div className="channels-sidebar">
-//       <div className="channels-header d-flex justify-content-between align-items-center mb-3">
-//         <h5 className="mb-0">{t('channels')}</h5>
-//         <Button variant="outline-primary" size="sm" onClick={onAddChannel}>+</Button>
-//       </div>
-//       <div className="channels-list-wrapper">
-//         <ListGroup variant="flush">
-//           {channels.map((channel) => {
-//             const isActive = channel.id === currentChannelId
-//
-//             return (
-//               // <ListGroup.Item
-//               //   key={channel.id}
-//               //   className="p-0 border-0"
-//               //   onClick={e => handleSelectChannel(channel.id, e)}
-//               // >
-//               //   <div className={`d-flex align-items-center w-100 rounded-0 text-start ${isActive ? 'bg-primary text-white' : 'bg-light'}`}>
-//               //     <span className="text-truncate ps-3 py-2 flex-grow-1">
-//               //       <span aria-hidden="true"># </span>
-//               //       {channel.name}
-//               //     </span>
-//               //
-//               //     {channel.removable && isActive && (
-//               //       <Dropdown onClick={e => e.stopPropagation()}>
-//               //         <Dropdown.Toggle as={CustomToggle}>
-//               //           <BsThreeDotsVertical />
-//               //         </Dropdown.Toggle>
-//               //
-//               //         <Dropdown.Menu>
-//               //           <Dropdown.Item onClick={() => onRename(channel)}>
-//               //             {t('channel.rename')}
-//               //           </Dropdown.Item>
-//               //           <Dropdown.Item onClick={() => onRemove(channel)}>
-//               //             {t('channel.remove')}
-//               //           </Dropdown.Item>
-//               //         </Dropdown.Menu>
-//               //       </Dropdown>
-//               //     )}
-//               //   </div>
-//               // </ListGroup.Item>
-//               <ListGroup.Item
-//                 key={channel.id}
-//                 className="p-0 border-0"
-//               >
-//                 <div className="d-flex align-items-center w-100">
-//                   <Button
-//                     onClick={e => handleSelectChannel(channel.id, e)}
-//                     variant={isActive ? 'primary' : 'light'}
-//                     className="text-start flex-grow-1 text-truncate rounded-0 ps-3 py-2 border-0"
-//                     role="button"
-//                   >
-//                     <span aria-hidden="true"># </span>
-//                     {channel.name}
-//                   </Button>
-//
-//                   {channel.removable && isActive && (
-//                     <Dropdown onClick={e => e.stopPropagation()}>
-//                       <Dropdown.Toggle as={CustomToggle}>
-//                         <BsThreeDotsVertical />
-//                       </Dropdown.Toggle>
-//
-//                       <Dropdown.Menu>
-//                         <Dropdown.Item onClick={() => onRename(channel)}>
-//                           {t('channel.rename')}
-//                         </Dropdown.Item>
-//                         <Dropdown.Item onClick={() => onRemove(channel)}>
-//                           {t('channel.remove')}
-//                         </Dropdown.Item>
-//                       </Dropdown.Menu>
-//                     </Dropdown>
-//                   )}
-//                 </div>
-//               </ListGroup.Item>
-//             )
-//           })}
-//         </ListGroup>
-//       </div>
-//     </div>
-//   )
-// }
-//
-// export default ChannelsList
 import React from 'react'
 import {
   ListGroup, Button, Dropdown,
@@ -388,11 +267,10 @@ const ChannelsList = ({
     }
   }
 
-  const CustomToggle = React.forwardRef(({ children, onClick, ...props }, ref) => (
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
     <a
       href="#"
       ref={ref}
-      {...props} // передаём aria-label и прочие атрибуты сюда
       onClick={(e) => {
         e.preventDefault()
         e.stopPropagation()
@@ -400,7 +278,7 @@ const ChannelsList = ({
       }}
       className="text-decoration-none text-secondary p-1"
     >
-      {/* Убираем visually-hidden, т.к. aria-label передаётся */}
+      <span className="visually-hidden">{t('channel.manage')}</span>
       <span className="ms-1 text-white"><BsCaretDownFill size="12" /></span>
       {children}
     </a>
@@ -413,28 +291,95 @@ const ChannelsList = ({
         <Button variant="outline-primary" size="sm" onClick={onAddChannel}>+</Button>
       </div>
       <div className="channels-list-wrapper">
+        {/* <ListGroup variant="flush"> */}
+        {/*  {channels.map((channel) => { */}
+        {/*    const isActive = channel.id === currentChannelId */}
+
+        {/*    return ( */}
+        {/*      // <ListGroup.Item */}
+        {/*      //   key={channel.id} */}
+        {/*      //   className="p-0 border-0" */}
+        {/*      //   onClick={e => handleSelectChannel(channel.id, e)} */}
+        {/*      // > */}
+        {/*      //   <div className={`d-flex align-items-center w-100 rounded-0 text-start ${isActive ? 'bg-primary text-white' : 'bg-light'}`}> */}
+        {/*      //     <span className="text-truncate ps-3 py-2 flex-grow-1"> */}
+        {/*      //       <span aria-hidden="true"># </span> */}
+        {/*      //       {channel.name} */}
+        {/*      //     </span> */}
+        {/*      // */}
+        {/*      //     {channel.removable && isActive && ( */}
+        {/*      //       <Dropdown onClick={e => e.stopPropagation()}> */}
+        {/*      //         <Dropdown.Toggle as={CustomToggle}> */}
+        {/*      //           <BsThreeDotsVertical /> */}
+        {/*      //         </Dropdown.Toggle> */}
+        {/*      // */}
+        {/*      //         <Dropdown.Menu> */}
+        {/*      //           <Dropdown.Item onClick={() => onRename(channel)}> */}
+        {/*      //             {t('channel.rename')} */}
+        {/*      //           </Dropdown.Item> */}
+        {/*      //           <Dropdown.Item onClick={() => onRemove(channel)}> */}
+        {/*      //             {t('channel.remove')} */}
+        {/*      //           </Dropdown.Item> */}
+        {/*      //         </Dropdown.Menu> */}
+        {/*      //       </Dropdown> */}
+        {/*      //     )} */}
+        {/*      //   </div> */}
+        {/*      // </ListGroup.Item> */}
+        {/*      <ListGroup.Item */}
+        {/*        key={channel.id} */}
+        {/*        className="p-0 border-0" */}
+        {/*      > */}
+        {/*        <div className="d-flex align-items-center w-100"> */}
+        {/*          <Button */}
+        {/*            onClick={e => handleSelectChannel(channel.id, e)} */}
+        {/*            variant={isActive ? 'primary' : 'light'} */}
+        {/*            className="text-start flex-grow-1 text-truncate rounded-0 ps-3 py-2 border-0" */}
+        {/*            role="button" */}
+        {/*          > */}
+        {/*            <span aria-hidden="true"># </span> */}
+        {/*            {channel.name} */}
+        {/*          </Button> */}
+
+        {/*          {channel.removable && isActive && ( */}
+        {/*            <Dropdown onClick={e => e.stopPropagation()}> */}
+        {/*              <Dropdown.Toggle as={CustomToggle}> */}
+        {/*                <BsThreeDotsVertical /> */}
+        {/*              </Dropdown.Toggle> */}
+
+        {/*              <Dropdown.Menu> */}
+        {/*                <Dropdown.Item onClick={() => onRename(channel)}> */}
+        {/*                  {t('channel.rename')} */}
+        {/*                </Dropdown.Item> */}
+        {/*                <Dropdown.Item onClick={() => onRemove(channel)}> */}
+        {/*                  {t('channel.remove')} */}
+        {/*                </Dropdown.Item> */}
+        {/*              </Dropdown.Menu> */}
+        {/*            </Dropdown> */}
+        {/*          )} */}
+        {/*        </div> */}
+        {/*      </ListGroup.Item> */}
+        {/*    ) */}
+        {/*  })} */}
+        {/* </ListGroup> */}
         <ListGroup variant="flush">
           {channels.map((channel) => {
             const isActive = channel.id === currentChannelId
 
             return (
-              <ListGroup.Item
-                key={channel.id}
-                className="p-0 border-0"
-                onClick={e => handleSelectChannel(channel.id, e)}
-              >
-                <div className={`d-flex align-items-center w-100 rounded-0 text-start ${isActive ? 'bg-primary text-white' : 'bg-light'}`}>
-                  <span className="text-truncate ps-3 py-2 flex-grow-1">
-                    <span aria-hidden="true"># </span>
+              <ListGroup.Item key={channel.id} className="p-0 border-0">
+                <li className="nav-item w-100 d-flex align-items-center">
+                  <button
+                    type="button"
+                    className={`w-100 rounded-0 text-start btn ${isActive ? 'btn-primary' : 'btn-light'}`}
+                    onClick={e => handleSelectChannel(channel.id, e)}
+                  >
+                    <span className="me-1">#</span>
                     {channel.name}
-                  </span>
+                  </button>
 
                   {channel.removable && isActive && (
                     <Dropdown onClick={e => e.stopPropagation()}>
-                      <Dropdown.Toggle
-                        as={CustomToggle}
-                        aria-label={t('channel.manage')}
-                      >
+                      <Dropdown.Toggle as={CustomToggle} aria-label={t('channel.manage')}>
                         <BsThreeDotsVertical />
                       </Dropdown.Toggle>
 
@@ -448,11 +393,12 @@ const ChannelsList = ({
                       </Dropdown.Menu>
                     </Dropdown>
                   )}
-                </div>
+                </li>
               </ListGroup.Item>
             )
           })}
         </ListGroup>
+
       </div>
     </div>
   )
