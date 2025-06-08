33dc1e9353a575809d454796b42c12f1a1efdaae
botti4elli <apple-003@mail.ru>
2025-06-08 21:25:13 +0400

    _

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,40 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { ToastContainer } from 'react-toastify'
+//
+// import routes from '../routes.jsx'
+// import Header from './Header.jsx'
+//
+// import 'react-toastify/dist/ReactToastify.css'
+//
+// const App = () => (
+//   <BrowserRouter>
+//     <Header />
+//     <Routes>
+//       {routes.map(({ path, element }) => (
+//         <Route key={path} path={path} element={element} />
+//       ))}
+//     </Routes>
+//
+//     <ToastContainer
+//       position="top-right"
+//       autoClose={5000}
+//       hideProgressBar={false}
+//       newestOnTop
+//       closeOnClick
+//       pauseOnFocusLoss
+//       draggable
+//       pauseOnHover
+//       progressClassName="bg-success"
+//     />
+//   </BrowserRouter>
+// )
+//
+// export default App
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-
 import routes from '../routes.jsx'
 import Header from './Header.jsx'
-
+import rollbar from '../rollbar/rollbarConfig.js'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => (
@@ -15,24 +46,20 @@ const App = () => (
       ))}
     </Routes>
 
-      function TestError() {
-      const a = null;
-      return a.hello();
-  }
-
-
-      {/* /!*  Кнопка для генерации ошибки *!/ */}
-    {/* <div className="text-center mt-4"> */}
-    {/*  <button */}
-    {/*    type="button" */}
-    {/*    className="btn btn-danger" */}
-    {/*    onClick={() => { */}
-    {/*      throw new Error('Test error from Rollbar button') */}
-    {/*    }} */}
-    {/*  > */}
-    {/*    Throw test error */}
-    {/*  </button> */}
-    {/* </div> */}
+    {/* Кнопка для отправки тестовой ошибки в Rollbar */}
+    {process.env.NODE_ENV !== 'test' && (
+      <div className="text-center mt-4">
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={() => {
+            rollbar.error(new Error('Test error sent manually to Rollbar'))
+          }}
+        >
+          Send test error
+        </button>
+      </div>
+    )}
 
     <ToastContainer
       position="top-right"
