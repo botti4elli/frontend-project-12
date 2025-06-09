5e0ee716eb3ecff2e6517887f904c12a2fcd72b4
botti4elli <apple-003@mail.ru>
2025-06-09 07:54:10 +0400

    _

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,17 +1,49 @@
+// import globals from 'globals'
+// import jsPlugin from '@eslint/js'
+// import stylistic from '@stylistic/eslint-plugin'
+//
+// export default [
+//   stylistic.configs.recommended,
+//   jsPlugin.configs.recommended,
+//   {
+//     files: ['**/*.js', '**/*.jsx'],
+//   },
+//   {
+//     ignores: ['dist/'],
+//   },
+//   {
+//     languageOptions: {
+//       globals: {
+//         ...globals.browser,
+//         ...globals.node,
+//         ...globals.es2021,
+//       },
+//       ecmaVersion: 'latest',
+//       sourceType: 'module',
+//       parserOptions: {
+//         ecmaFeatures: {
+//           jsx: true,
+//         },
+//       },
+//     },
+//     rules: {
+//       'no-console': 'off',
+//     },
+//   },
+// ]
 import globals from 'globals'
 import jsPlugin from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
 
 export default [
   stylistic.configs.recommended,
   jsPlugin.configs.recommended,
   {
     files: ['**/*.js', '**/*.jsx'],
-  },
-  {
-    ignores: ['dist/'],
-  },
-  {
+    plugins: {
+      react: reactPlugin,
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -28,6 +60,13 @@ export default [
     },
     rules: {
       'no-console': 'off',
+      // Правила из плагина react, например:
+      'react/react-in-jsx-scope': 'off', // если используешь React 17+ без импорта React
+      'react/jsx-uses-react': 'off', // тоже для нового JSX трансформа
+      'react/jsx-uses-vars': 'error', // помечать JSX компоненты как используемые
     },
+  },
+  {
+    ignores: ['dist/'],
   },
 ]
