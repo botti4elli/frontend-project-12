77b72fc02b6ad55982f1deecdcc9f6da921aa2c6
botti4elli <apple-003@mail.ru>
2025-06-09 08:18:06 +0400

    final

--- README.md
+++ README.md
@@ -9,3 +9,26 @@
 [![Maintainability](https://qlty.sh/badges/531f6aba-2132-4b43-991b-a8fb875ad520/maintainability.svg)](https://qlty.sh/gh/botti4elli/projects/frontend-project-12)
 ### Github Actions
 [![Node CI](https://github.com/botti4elli/frontend-project-12/actions/workflows/nodeci.yml/badge.svg)](https://github.com/botti4elli/frontend-project-12/actions/workflows/nodeci.yml)
+
+#### Общение разделено на каналы. Каждый канал — это отдельный чат. Слева список доступных каналов, к которым можно присоединиться. Можно создать свой канал.
+#### По умолчанию вы автоматически добавлены в **#general**. Это общий канал.
+#### В разделе **Сообщения** можно общаться с другими пользователями, зарегистрированными в  чате. Здесь будет отображаться список ваших последних диалогов.
+
+
+## Установка 
+1. Клонируйте репозиторий:
+```sh 
+git clone https://github.com/botti4elli/frontend-project-12
+```
+
+2. Установите зависимости:
+
+```sh
+make install
+```
+
+3. Запуск приложения локально:
+
+```sh
+make start
+```
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,36 +1,3 @@
-// import globals from 'globals'
-// import jsPlugin from '@eslint/js'
-// import stylistic from '@stylistic/eslint-plugin'
-//
-// export default [
-//   stylistic.configs.recommended,
-//   jsPlugin.configs.recommended,
-//   {
-//     files: ['**/*.js', '**/*.jsx'],
-//   },
-//   {
-//     ignores: ['dist/'],
-//   },
-//   {
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//         ...globals.es2021,
-//       },
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//       parserOptions: {
-//         ecmaFeatures: {
-//           jsx: true,
-//         },
-//       },
-//     },
-//     rules: {
-//       'no-console': 'off',
-//     },
-//   },
-// ]
 import globals from 'globals'
 import jsPlugin from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
@@ -60,10 +27,9 @@ export default [
     },
     rules: {
       'no-console': 'off',
-      // Правила из плагина react, например:
-      'react/react-in-jsx-scope': 'off', // если используешь React 17+ без импорта React
-      'react/jsx-uses-react': 'off', // тоже для нового JSX трансформа
-      'react/jsx-uses-vars': 'error', // помечать JSX компоненты как используемые
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
     },
   },
   {
